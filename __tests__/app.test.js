71bba5acd7285eb6f2c59e0d650f12491b99c707
Ben Waples <66393111+benwaples@users.noreply.github.com>
2020-11-12 17:39:11 -0800

    Auth Routes (#1)
    * User model insert, setup-sql, user-service
    
    * npm packages, ensure-auth, authController
    
    * signup route
    
    * login route
    
    * notes for login test
    
    * update yml
    
    Co-authored-by: Thomas Stussi <tstussi06@gmail.com>

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,41 @@ const app = require('../lib/app');
 
 describe('Canary-BE routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
+
+  it('should insert a user into the DB via POST', async() => {
+    const user = {
+      userName: 'benwa',
+      password: '1234',
+      userRole: 'student'
+    };
+
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .then(res => expect(res.body).toEqual({ id: expect.any(String), userName: 'benwa', userRole: 'student' }));
+  });
+
+  // test for same userName signups
+
+  it('should login a user via POST', async() => {
+    const user = {
+      userName: 'benwa',
+      password: '1234',
+      userRole: 'student'
+    };
+
+    await request(app)
+      .post('/api/v1/auth/signup')
+      .send(user);
+
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send(user)
+      .then(res => expect(res.body).toEqual({ id: expect.any(String), userName: 'benwa', userRole: 'student' }));
+  });
+
+  // test for failed login
+  // it('should fail to signup a user who is already loged in');
 });
