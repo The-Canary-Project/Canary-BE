a464603b5e8fd799854bad408462ec690464ca49
bwapes <benwaples@gmail.com>
2020-11-12 16:56:03 -0800

    login route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -20,4 +20,21 @@ describe('Canary-BE routes', () => {
       .send(user)
       .then(res => expect(res.body).toEqual({ id: expect.any(String), userName: 'benwa', userRole: 'student' }));
   });
+
+  it('should login a user via POST', async() => {
+    const user = {
+      userName: 'benwa',
+      password: '1234',
+      userRole: 'student'
+    };
+
+    await request(app)
+      .post('/api/v1/auth/signup')
+      .send(user);
+
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send(user)
+      .then(res => expect(res.body).toEqual({ id: expect.any(String), userName: 'benwa', userRole: 'student' }));
+  });
 });
