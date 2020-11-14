5677e8470d9be13da2ca4d23893e5381df5a83ca
Thomas R Stussi <tstussi06@gmail.com>
2020-11-13 17:27:28 -0800

    Setup db (#2)
    * User model insert, setup-sql, user-service
    
    * npm packages, ensure-auth, authController
    
    * signup route
    
    * login route
    
    * notes for login test
    
    * update yml
    
    * update .env
    
    * add duplicate username test
    
    * add invalid username/password test
    
    * add verify route test
    
    * CASCADE
    
    * prompts table in setup.sql added
    
    * Prompt model insert method
    
    * seed script added
    
    * remove console.log
    
    Co-authored-by: bwapes <benwaples@gmail.com>
    Co-authored-by: katiePDX <61799774+katiepdx@users.noreply.github.com>

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,117 @@ const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('Canary-BE routes', () => {
+describe('Canary-BE users routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('should insert a user into the DB via POST', async() => {
+    const user = {
+      userName: 'benwa',
+      password: '1234',
+      userRole: 'student'
+    };
+
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .then(res => expect(res.body).toEqual({ id: expect.any(String), userName: 'benwa', userRole: 'student' }));
+  });
+
+  // test for same userName signups
+  it('should throw an error if a user signs up with an already taken username', async() => {
+    const user = {
+      userName: 'benwa',
+      password: '1234',
+      userRole: 'student'
+    };
+
+    await request(app)
+      .post('/api/v1/auth/signup')
+      .send(user);
+
+    return await request(app)
+      .post('/api/v1/auth/signup')
+      .send(user)
+      .then(res => expect(res.body.message).toEqual('duplicate key value violates unique constraint \"users_user_name_key\"'));
+  });
+
+  it('should login a user via POST', async() => {
+    const user = {
+      userName: 'benwa',
+      password: '1234',
+      userRole: 'student'
+    };
+
+    await request(app)
+      .post('/api/v1/auth/signup')
+      .send(user);
+
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send(user)
+      .then(res => expect(res.body).toEqual({ id: expect.any(String), userName: 'benwa', userRole: 'student' }));
+  });
+
+  it('should throw an error if a user gives an incorrect password or username', async() => {
+    const user = {
+      userName: 'benwa',
+      password: '1234',
+      userRole: 'student'
+    };
+
+    await request(app)
+      .post('/api/v1/auth/signup')
+      .send(user);
+
+    await request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        userName: 'benwa',
+        password: '1236'
+      })
+      .then(res => expect(res.body.message).toEqual('Invalid username/password'));
+
+    await request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        userName: 'benwaaa',
+        password: '1234'
+      })
+      .then(res => expect(res.body.message).toEqual('Invalid username/password'));
+  });
+
+  it('test that a signed up user has a jwt', async() => {
+    const user = {
+      userName: 'benwa',
+      password: '1234',
+      userRole: 'student'
+    };
+
+    const newUser = await request(app)
+      .post('/api/v1/auth/signup')
+      .send(user);
+
+    return expect(newUser.header['set-cookie'][0]).toEqual(expect.any(String));
+  });
+
+  it('test that a logged in user has a jwt', async() => {
+    const user = {
+      userName: 'benwa',
+      password: '1234',
+      userRole: 'student'
+    };
+
+    await request(app)
+      .post('/api/v1/auth/signup')
+      .send(user);
+
+    const newUser = await request(app)
+      .post('/api/v1/auth/login')
+      .send(user);
+
+    return expect(newUser.header['set-cookie'][0]).toEqual(expect.any(String));
   });
 
   it('should insert a user into the DB via POST', async() => {
