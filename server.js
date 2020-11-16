3626a1f659b1661a1d5c56ccf2130dd82912e56b
Thomas R Stussi <tstussi06@gmail.com>
2020-11-16 11:57:23 -0800

    Socket server (#3)
    * User model insert, setup-sql, user-service
    
    * npm packages, ensure-auth, authController
    
    * signup route
    
    * login route
    
    * notes for login test
    
    * update yml
    
    * update .env
    
    * add duplicate username test
    
    * add invalid username/password test
    
    * add verify route test
    
    * CASCADE
    
    * prompts table in setup.sql added
    
    * Prompt model insert method
    
    * seed script added
    
    * remove console.log
    
    * socket server
    
    * socket.io server
    
    Co-authored-by: bwapes <benwaples@gmail.com>
    Co-authored-by: katiePDX <61799774+katiepdx@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,11 +1,46 @@
 const app = require('./lib/app');
 const pool = require('./lib/utils/pool');
+const socket = require('socket.io');
 
 const PORT = process.env.PORT || 7890;
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
+});
+
+const io = socket(app.listen(8080, () => {
+  console.log('Started on 8080');
+}));
+
+//Socket Connection
+io.on('connection', (socket) => {
+  console.log(socket.id, 'SOCKET ID');
+
+  //Rooms
+  let room = '';
+  socket.on('ROOM', (data) => {
+    room = data;
+
+    socket.join(room);
+    io.sockets.in(room).emit('connectToRoom', `You are in ${room}'s Classroom`);
+  });
+  //Send List of Rooms
+  socket.on('GET_ROOMS', () => {
+    socket.emit('ROOM_LIST', io.sockets.adapter.rooms);
+  });
+
+  //Messaging
+  socket.on('SEND_MESSAGE', (data) => {
+    io.sockets.in(room.emit('RECEIVE_MESSAGE', data));
+    console.log(`${data.author} joined the chat`);
+  });
+
+  socket.on('SEND_QUESTION', (data) => {
+    console.log(data, 'SEND_QUESTION');
+    io.sockets.in(room).emit(
+      'RECEIVE_QUESTION', data
+    );
+  });
 });
 
 process.on('exit', () => {
